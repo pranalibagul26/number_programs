ba991aa7d2918e151f36e86c3b62113a114590cc
Pranali <154823375+pranalibagul26@users.noreply.github.com>
2024-01-12 00:37:25 +0530

    Add files via upload

---
+++ spy_num.java
@@ -0,0 +1,27 @@
+class spy_num
+{
+public static void main(String[]args)
+{
+                                                                               
+ int n=132;
+ int sum=0;
+ int prod=1;
+
+while(n>0)//132>0
+{
+ int rem=n%10;          //2 //5 //1 //ek ek digit nikalna hai
+ sum=sum+rem;           //0+2=2 //2+5=7 //
+ prod=prod*rem;         //1*2=2 //2*5=6
+ n=n/10;           	 //updating value of n to come out of loop
+}
+
+if(sum==prod)
+{
+System.out.println(sum+" = "+prod);
+}
+else
+{
+System.out.println("not spy number");
+}
+}
+}
